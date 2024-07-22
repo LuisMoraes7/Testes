78cda1d60f23701ac0447dd247a8e29970877af8
Luís Eduardo <luisedunascimento17@gmail.com>
2024-07-22 15:42:10 -0300

    Atualização em playersPL

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-function inHTML(valor){
-  const img = document.createElement('img')
-  img.src = valor
-  document.body.appendChild(img)
 
-}
-
-
- fetch('https://luismoraes7.github.io/Testes/playersPL.json')
+fetch('https://luismoraes7.github.io/Testes/playersPL.json')
    .then(response => response.json())
    .then(players => {
     players.map((player) => {if (player.team === "Arsenal" && player.position === "Defender"){
@@ -16,8 +9,5 @@ function inHTML(valor){
       
     }}
   )
-    
-
-})
-
-
+}
+)
